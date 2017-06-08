0c5215e36c33c10155cf77ee4aeef010638d4afa
Alify Ferreira Pires <alifyz@Alifyz.local>
2017-06-07 23:46:00 -0300

    Tesing First Approach

--- adjacentElementsProduct/AdjacentsArrays/Program.cs
+++ adjacentElementsProduct/AdjacentsArrays/Program.cs
@@ -6,7 +6,25 @@ namespace AdjacentsArrays
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] Array = { 1, -2, 4, 7, 6, 5, 9, 10 };
+            Console.WriteLine(arrayPset(Array));
+        }
+
+
+        public static int arrayPset(int[] inputArray) {
+            int result = 0;
+
+            for (int k = 0; k < inputArray.Length; k++) {
+
+                if(inputArray[k] * inputArray[k+1] > result) {
+                    result = inputArray[k] * inputArray[k + 1];
+                }
+
+            }
+
+
+            return result;
+
         }
     }
 }
