ab06478ef0f9b13d819af1f1e212ed0ea92d231c
Alify Ferreira Pires <alifyz@Alifyz.local>
2017-06-07 23:30:29 -0300

    Initial Project

---
+++ adjacentElementsProduct/AdjacentsArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdjacentsArrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
